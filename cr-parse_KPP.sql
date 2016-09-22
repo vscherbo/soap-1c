121f13392b48bcdfd67a8588bd6cb4ecd1ecf66c
Vladimir Scherbo <vscherbo@gmail.com>
2016-09-22 09:16:49 +0300

    возврат флага завершения

--- cr-parse_KPP.sql
+++ cr-parse_KPP.sql
@@ -1,11 +1,18 @@
-CREATE OR REPLACE FUNCTION arc_energo.parse_KPP(
-    IN in_xml character varying)
-  RETURNS varchar AS
+-- Function: parse_kpp(character varying)
+
+-- DROP FUNCTION parse_kpp(character varying);
+
+CREATE OR REPLACE FUNCTION parse_kpp(
+    IN in_xml character varying,
+    OUT ret_kpp_flg boolean,
+    OUT ret_kpp character varying)
+  RETURNS record AS
 $BODY$
 from xml.etree import ElementTree as ET
 
 root = ET.fromstring(in_xml)
 
+ret_kpp_flg = False
 ret_kpp = 'not found'
 # python 2.7 for elem in root.iter():
 for elem in root.getiterator(): #python 2.6
@@ -15,9 +22,12 @@ for elem in root.getiterator(): #python 2.6
             #    plpy.notice(a1.encode('utf-8'));
             kpp = el.attrib.get('КПП'.decode('utf-8'))
             if kpp is not None:
+                ret_kpp_flg = True
                 ret_kpp = kpp
 
-return ret_kpp
+return ret_kpp_flg, ret_kpp
 $BODY$
   LANGUAGE plpython2u VOLATILE
   COST 100;
+ALTER FUNCTION parse_kpp(character varying)
+  OWNER TO postgres;
