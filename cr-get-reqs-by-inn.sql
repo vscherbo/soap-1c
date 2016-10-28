2f891825044148a0cc344caac274bf080fbbbc86
Vladimir Scherbo <vscherbo@gmail.com>
2016-10-28 15:46:07 +0300

    поддержка ИНН-10 и ИНН-12

--- cr-get-reqs-by-inn.sql
+++ cr-get-reqs-by-inn.sql
@@ -2,7 +2,7 @@
 
 -- DROP FUNCTION public.get_reqs_by_inn(character varying);
 
-CREATE OR REPLACE FUNCTION public.get_reqs_by_inn(
+CREATE OR REPLACE FUNCTION public.get_reqs_by_inn_debug(
     IN inn character varying,
     OUT ret_flg boolean,
     OUT ret_txt character varying)
@@ -15,13 +15,22 @@ from datetime import datetime
 #flog.write("Start at " + str(datetime.now()) +'\n')
 good_status = [ 200, 500 ]
 
+if len(inn.encode('utf8')) == 10: # Corporation
+   NameSpace = u"getCorporationRequisitesByINN"
+elif len(inn.encode('utf8')) == 12: # Entrepreneur
+   NameSpace = u"getEntrepreneurRequisitesByINN"
+else:
+   ret_flg = False
+   ret_txt = 'Допустимая длина ИНН 10 или 12 цифр.'
+   return ret_flg, ret_txt
+
 url_1c_api = u'https://api.orgregister.1c.ru/orgregister/v2?wsdl'
 sess = requests.Session()
 sess.auth = ('TDEnergoService', 'energ0serv1ce')
 sess.headers.update({'SOAPAction': u'""', 'Content-Type': 'text/xml; charset=utf-8', 'Content-type': 'text/xml; charset=utf-8', 'Soapaction': u'""'})
 
-xml_prefix=u'<?xml version="1.0" encoding="UTF-8"?><SOAP-ENV:Envelope xmlns:ns0="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ns1="http://ws.orgregister.company1c.com/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/"><SOAP-ENV:Header/><ns0:Body><ns1:getCorporationRequisitesByINN><ns1:INN>'
-xml_suffix=u'</ns1:INN></ns1:getCorporationRequisitesByINN></ns0:Body></SOAP-ENV:Envelope>'
+xml_prefix=u'<?xml version="1.0" encoding="UTF-8"?><SOAP-ENV:Envelope xmlns:ns0="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ns1="http://ws.orgregister.company1c.com/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/"><SOAP-ENV:Header/><ns0:Body><ns1:' + NameSpace + u'><ns1:INN>'
+xml_suffix=u'</ns1:INN></ns1:' + NameSpace + u'></ns0:Body></SOAP-ENV:Envelope>'
 
 xml_data = xml_prefix + inn.encode('utf8') +  xml_suffix
 #flog.write("xml_data=" + xml_data +'\n')
@@ -43,5 +52,3 @@ return ret_flg, ret_txt
 $BODY$
   LANGUAGE plpython2u VOLATILE
   COST 100;
-ALTER FUNCTION public.get_reqs_by_inn(character varying)
-  OWNER TO postgres;
